31afbc635197ba52a35f591b5a4efea03f97cd5b
yesenia <130951872+yesiviola@users.noreply.github.com>
2023-08-27 04:07:05 -0300

    Update Ejercicio 02.js

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -13,14 +13,48 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+constructor (nombre,apellido,edad,domicilio){
+      this.nombre=nombre;
+      this.apellido= apellido;
+      this.edad=edad;
+      this.domicilio= domicilio;
+   }
+   detalle (){
+      return {
+         nombre : this.nombre,
+         apellido: this.apellido,
+         edad: this.edad,
+         domicilio: this.domicilio
+      };
+   }
 }
+const persona1 = new Persona ("Juan","Perez", 30, "calle 234");
+const detallesPersona1 = persona1.detalle();
+console.log (detallesPersona1);
+
 
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+ Persona.prototype.datos = function () {
+      return `${this.nombre}, ${this.edad} años`;
+   };
 }
+function Persona(nombre, edad) {
+   this.nombre = nombre;
+   this.edad = edad;
+}
+
+agregarMetodo();
+
+const persona = new Persona("Juan", 22);
+console.log(persona.datos());
+
+
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
